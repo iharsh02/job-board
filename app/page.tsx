12c9e270a0c936f6196deb6fedfb6468f713a7ba
Senp1 <harshthakur0025@gmail.com>
2024-08-07 14:30:44 +0000

    testing ci

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { SiteHeader } from "@/components/SiteHeader";
 
 const HomePage = () => {
   return (
-    <main className="w-full max-h-screen">
+    <main className="w-full max-h-screen"
       <div className="absolute inset-0 h-full w-full flex flex-col items-center gap-24 p-4 bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
         <SiteHeader />
         <section className="flex flex-col justify-start items-center gap-8">
